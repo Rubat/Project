4ac277b5cb9b93cc17f16fca3db58b7ca73673fd
Rubat Majeed <rubata=majeed1@gmail.com>
2020-01-10 02:14:59 +0500

    A

--- 3.py
+++ 3.py
@@ -1,149 +1,67 @@
-#Importing the Libraries
-import cv2
 import numpy as np
-from skimage import measure
-from math import floor
+import cv2
 
-#Reading 2 Frames
-image1 = cv2.imread('1.jpg')
-image2 = cv2.imread('190.jpg')
+#flags = [i for i in dir(cv2) if i.startswith('COLOR_G')]
+#print(flags)
 
-#Converting the Frames into Gray Scale
-image3 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-image4 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+img = cv2.imread('1.jpg', 1)
+hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+#cv2.imshow('1', hsv)
+#cv2.waitKey(50000) & 0xFF
 
-#Subtracting The Frames
-sub = cv2.subtract(image3, image4)
+# define range of red color in HSV
+lower_red = np.array([110, 50, 50])
+upper_red = np.array([130, 255, 255])
 
-#Binary threshing of Subtracted Image
-ret1, th1 = cv2.threshold(sub, 65, 255, cv2.THRESH_BINARY)
+# define range of orange color in HSV
+lower_orange = np.array([91, 50, 50])
+upper_orange = np.array([111, 255, 255])
 
-#Find Connected pixels in the Threshed Image
-thresh = th1
-labels = measure.label(thresh, neighbors=8, background=0)
-mask = np.zeros(thresh.shape, dtype="uint8")
+# define range of brown color in HSV
+lower_brown = np.array([102, 50, 50])
+upper_brown = np.array([122, 255, 255])
 
-#Finding Pixels having Non-Zero Value
-for label in np.unique(labels):
-    if label == 0:
-        continue
-    labelmask = np.zeros(thresh.shape, dtype="uint8")
-    labelmask[labels == label] = 255
-    numPixels = cv2.countNonZero(labelmask)
+# define range of green color in HSV
+lower_green = np.array([50, 100, 100])
+upper_green = np.array([70, 255, 255])
 
-    #Setting the Value of Connected Pixels in the Threshed Image
-    if numPixels > 700:
-        mask = cv2.add(mask, labelmask)
+# define range of blue color in HSV
+lower_blue = np.array([154, 2, 21])
+upper_blue = np.array([174, 255, 255])
 
-#Resolution of Image
-height = mask.shape[0]
-width = mask.shape[1]
+# Threshold the HSV image to get only blue colors
+mask_red = cv2.inRange(hsv, lower_red, upper_red)
+mask_orange = cv2.inRange(hsv, lower_orange, upper_orange)
+mask_brown = cv2.inRange(hsv, lower_brown, upper_brown)
+mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+mask_green = cv2.inRange(hsv, lower_green, upper_green)
+#mask = mask_red + mask_orange + mask_brown  + mask_blue
 
-#Creating two new arrays
-arr = np.array([])
-arr1 = np.array([])
+# Bitwise-AND mask and original image
+res1 = cv2.bitwise_and(img,img, mask= mask_red)
+res2 = cv2.bitwise_and(img,img, mask= mask_orange)
+res3 = cv2.bitwise_and(img,img, mask= mask_brown)
+res4 = cv2.bitwise_and(img,img, mask= mask_blue)
+res5 = cv2.bitwise_and(img,img, mask= mask_green)
+#res = res1 + res2 + res3 + res4
 
-#Finding the Index of Rows and Columns having value of 255
-for c in range(0, height):
-    for d in range(0, width):
-        if mask[c, d] == 255:
-            arr = np.append(arr, [c])
-            arr1 = np.append(arr1, [d])
+cv2.imshow('frame',img)
+#cv2.imshow('frame',img)
+#cv2.imshow('frame',img)
+#cv2.imshow('frame',img)
+#cv2.imshow('frame',img)
 
-#For Rows
-arr = np.unique(arr)
-#print(arr)
-a = arr.size
-#Bottom Left
-w = (np.amax(arr))
-w = floor(w)
-#Left Top
-y = (np.amin(arr1))
-y = floor(y)
+cv2.imshow('mask',mask_red)
+#cv2.imshow('mask',mask_orange)
+#cv2.imshow('mask',mask_brown)
+#cv2.imshow('mask',mask_blue)
+#cv2.imshow('mask',mask_green)
 
-#For Columns
-arr1 = np.unique(arr1)
-#print(arr1)
-b = arr1.size
-#Right Top
-h = (np.amax(arr1))
-h = floor(h)
-#Top
-x = (np.amin(arr))
-x = floor(x)
+cv2.imshow('res',res1)
+#cv2.imshow('res',res2)
+#cv2.imshow('res',res3)
+#cv2.imshow('res',res4)
+#cv2.imshow('res',res5)
 
-#expansion of rectangle
-e = 25
-f = e+1
 
-hh = (h-y)
-ww = (w-x)
-
-#Creating the Rectangle along the Detected Area
-cv2.rectangle(image1, (y-f, x-f), (h+e, w+e), (0, 0, 0), 1)
-
-#Rectangle width
-q = y-f
-t = h+e
-Rectangle_width = t-q
-print(Rectangle_width)
-
-#Rectangle Height
-r = x-f
-u = w+e
-if u > 250:
-    u = 230
-Rectangle_Height = u-r
-print(Rectangle_Height)
-
-#Crop the rectangle
-#cropped = image1[x-e:w+e, y-e:h+e]
-
-roi = image1[r:u, q:t]
-black_bg = 0*np.ones_like(image1)
-
-#cv2.rectangle(res, (x, y), (x + w, y + h), (0, 0, 0), 1)
-#--- paste ROIs on image with white background
-black_bg[r:u, q:t] = roi
-
-image1 = black_bg
-
-#Editing Image1
-new_image = np.zeros(image1.shape, image1.dtype)
-
-alpha = 1.5
-beta = 42
-gamma = 1.7
-
-#new_image = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
-
-lookUpTable = np.empty((1, 256), np.uint8)
-
-for i in range(256):
-    lookUpTable[0, i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
-
-res = cv2.LUT(image1, lookUpTable)
-
-hsv = cv2.cvtColor(res, cv2.COLOR_BGR2HSV)
-
-lower_red = np.array([175, 150, 48])
-upper_red = np.array([185, 255, 255])
-mask = cv2.inRange(hsv, lower_red, upper_red)
-
-#Finding Contours
-contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-#Creating Rectangle around the the Contours
-for c in contours:
-    # get the bounding rect
-    x, y, w, h = cv2.boundingRect(c)
-    # get the min area rect
-    rect = cv2.minAreaRect(c)
-    #Condition Width should be greater or equal to 10 Pixels
-    if rect[1][1] >= 10:
-        #Creating the Rectangle
-        cv2.rectangle(res, (x, y), (x + w, y + h), (0, 0, 255), -1)
-
-#Printing the Result
-cv2.imshow("Mask", res)
-cv2.waitKey(0)
+k = cv2.waitKey(0) & 0xFF
--- module1.py
+++ module1.py
@@ -2,28 +2,30 @@ import cv2
 import numpy as np
 
 # Reading Video
-cap = cv2.VideoCapture('5.mp4')
+cap = cv2.VideoCapture('1.mp4')
 
-#Background Subtraction
+# Background Subtraction
 mask = cv2.createBackgroundSubtractorMOG2(history=1, varThreshold=15, detectShadows=False)
-#Making matrix for Erosion, dilation and morphing
-kernel = np.ones((2,2),np.uint8)
-kernel1 = np.ones((1,2),np.uint8)
+# Making matrix for Erosion, dilation and morphing
+kernel = np.ones((2, 2), np.uint8)
+kernel1 = np.ones((1, 2), np.uint8)
 
-#This is where the video is read
+# This is where the video is read
 while cap.isOpened():
 
     ret, frame = cap.read()
     if not ret:
         break
+    frame1 = frame
+
     mask1 = mask.apply(frame)
     # Erosion
     mask1 = cv2.erode(mask1, kernel, iterations=1)
     # Dialtion
     mask1 = cv2.dilate(mask1, kernel1, iterations=3)
     # Morphing
-    #mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, kernel)
-    #mask1 = cv2.morphologyEx(mask1, cv2.MORPH_CLOSE, kernel)
+    # mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, kernel)
+    # mask1 = cv2.morphologyEx(mask1, cv2.MORPH_CLOSE, kernel)
 
     contours, _ = cv2.findContours(mask1.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -34,19 +36,47 @@ while cap.isOpened():
             (x, y, w, h) = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x + int(w), y + int(h)), (255, 255, 255), 2)
 
+            upper_left = (x, y)
+            bottom_right = (x + w, y + h)
+            frame1 = frame[upper_left[1]: bottom_right[1], upper_left[0]: bottom_right[0]]
+            # roi = frame[y:y+h, x:x+w]
+            # black_bg = 0 * np.ones_like(frame)
+            # black_bg[y:y+h, x:x+w] = roi
 
-            roi = frame[y:y+h, x:x+w]
-            black_bg = 0 * np.ones_like(frame)
-            black_bg[y:y+h, x:x+w] = roi
+            blurred_frame = cv2.GaussianBlur(frame1, (5, 5), 0)
+            # hsv1 = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2RGB)
+            hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+
+            lower_red = np.array([38, 86, 0])
+            upper_red = np.array([121, 255, 255])
+            mask2 = cv2.inRange(hsv, lower_red, upper_red)
+
+            # Erosion
+            mask2 = cv2.erode(mask2, kernel, iterations=4)
+            # Dialtion
+            mask2 = cv2.dilate(mask2, kernel1, iterations=4)
+            # Morphing
+            # mask2 = cv2.morphologyEx(mask2, cv2.MORPH_OPEN, kernel)
+            # mask2 = cv2.morphologyEx(mask2, cv2.MORPH_CLOSE, kernel)
+
+            contours, _ = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+            for contour in contours:
+                cv2.drawContours(frame1, contour, -1, (0, 0, 0), 1)
+
+            for b in contours:
+                # get the bounding rect
+                x, y, w, h = cv2.boundingRect(b)
+                # draw a green rectangle to visualize the bounding rect2
+                cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 1)
 
     # cv2.imshow('result', mask1)
+    # cv2.imshow('result', mask2)
     cv2.imshow('result', frame)
     # cv2.imshow('result', black_bg)
-    k = cv2.waitKey(50) & 0xFF
+    k = cv2.waitKey(100) & 0xFF
     if k == 27:
         break
 
-
 cap.release()
 cv2.destroyAllWindows()
-
